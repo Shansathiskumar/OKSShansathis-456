e7e9f7fcdecec9b7851ec1bb5e0f74684824f412
Shansathiskumar <er.shansathis456@gmail.com>
2021-12-31 16:31:19 +0530

    userName Funtionality

--- src/com/java/Project.java
+++ src/com/java/Project.java
@@ -1,5 +1,11 @@
 package com.java;
 
 public class Project {
+	
+	public static void userName() {
+System.out.println("username");
+		}
+public static void main(String[] args) {
+	userName();
+}}
 
-}
