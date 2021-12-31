183fc3f4987241896ed2c2e043e2558b65e630ac
Shansathiskumar <er.shansathis456@gmail.com>
2021-12-31 16:48:40 +0530

    passWord Funtionality

--- src/com/java/Project.java
+++ src/com/java/Project.java
@@ -2,4 +2,19 @@ package com.java;
 
 public class Project {
 
+	public static void passWord() {
+System.out.println("password");
+	}
+	public static void main(String[] args) {
+		
+		passWord();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
